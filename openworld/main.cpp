bfdc68f1a7850aee1f9e505386bcc25f7d9eae44
martin dionisi <martindionisi@10.0.0.6>
2015-02-25 23:37:38 -0300

    Initial Commit

---
+++ openworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openworld
+//
+//  Created by martin dionisi on 25/2/15.
+//  Copyright (c) 2015 martin dionisi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
